3a9dba0045763826f1bcca2256ea61c098f3c465
marvin <marvin@ecwid.com>
2015-11-13 16:27:06 +0300

    Added fallback to file_get_contents

--- lib/ecwid_platform.php
+++ lib/ecwid_platform.php
@@ -19,7 +19,6 @@ class EcwidPlatform {
 
 	static public function fetch_url($url)
 	{
-
         $use_file_get_contents = get_option('ecwid_fetch_url_use_file_get_contents', false);
 
         if ($use_file_get_contents == 'Y') {
@@ -40,11 +39,21 @@ class EcwidPlatform {
             'data' => $result['body']
           );
         } elseif (is_object($result)) {
+
           $return = array(
             'code' => $result->get_error_code(),
             'data' => $result->get_error_data(),
             'message' => $result->get_error_message()
           );
+
+            $get_contents = file_get_contents($url);
+            if ($get_contents !== false) {
+                $return = array(
+                    'code' => 200,
+                    'data' => $get_contents,
+                    'is_file_get_contents' => true
+                );
+            }
         }
 
         return $return;
