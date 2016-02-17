8cfe9f5c21a94eeaff39b6730716f874c9e60612
marvin <marvin@ecwid.com>
2016-02-17 09:42:05 +0300

    small fix

--- includes/oembed.php
+++ includes/oembed.php
@@ -1,32 +1,33 @@
 <?php
 
-add_filter('embed_content', 'ecwid_embed_content', 10, 1);
+add_filter('embed_content', 'ecwid_oembed_content', 10, 1);
 
-function ecwid_embed_content($data)
+function ecwid_oembed_content($data)
 {
-	return $data;
+	echo ecwid_get_embed_content();
 }
 
-function ecwid_oembed_response_data($data)
+function ecwid_get_embed_content()
 {
 
+	$html = '';
 	$root_category_id = 0;
 
 	$post_content = get_post(get_the_ID())->post_content;
 	$shortcodes = ecwid_find_shortcodes($post_content, 'ecwid');
 
 	if (!$shortcodes || !isset($shortcodes[0]) || !isset($shortcodes[0][3])) {
-		return $data;
+		return;
 	}
 
 	$attributes = $shortcodes[0][3];
 	if (!preg_match('/default_category_id=.([\\d]*)./', $attributes, $matches)) {
-		return $data;
+		return;
 	}
 
 	$root_category_id  = 0;
 	if (!is_numeric($matches[1])) {
-		return $data;
+		return;
 	} else if (isset($matches[1])) {
 		$root_category_id  = $matches[1];
 	}
@@ -37,13 +38,14 @@ function ecwid_oembed_response_data($data)
 		$categories = _ecwid_find_category_in_horizontal_categories_tree($categories, $root_category_id);
 	}
 
+	$result = '';
 	if (!empty($categories)) {
-		echo '<ul>';
+		$result .= '<ul>';
 		foreach ($categories as $category) {
 			$url = ecwid_get_category_url(array('id' => $category->id, 'url' => $category->link));
-			echo '<li><a href="' . $url . '">' . $category->name . '</a></li>';
+			$result .= '<li><a href="' . $url . '">' . $category->name . '</a></li>';
 		}
-		echo '</ul>';
+		$result .= '</ul>';
 	}
 
 	if (ecwid_is_paid_account()) {
@@ -52,15 +54,16 @@ function ecwid_oembed_response_data($data)
 		$products = $api->get_products(array('category' => $root_category_id));
 
 		if ($products) {
-			echo '<ul>';
+			$result .= '<ul>';
 			foreach ($products as $product) {
 				$url = ecwid_get_product_url(array('id' => $product->id, 'url' => $product->url));
-				echo '<li><a href="' . $url . '">' . $product->name . '</a></li>';
+				$result .= '<li><a href="' . $url . '">' . $product->name . '</a></li>';
 			}
-			echo '</ul>';
+			$result .= '</ul>';
 		}
 	}
- 	//die(var_dump($categories));
+
+	return $result;
 }
 
 function _ecwid_find_category_in_horizontal_categories_tree($categories, $root_id) {
