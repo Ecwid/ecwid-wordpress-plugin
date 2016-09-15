a97b4431fd692a08570ea509e9a5e32484c0eb9d
marvin <marvin@ecwid.com>
2016-09-15 04:09:16 +0400

    fix for typos that somehow reached master

--- lib/ecwid_api_v3.php
+++ lib/ecwid_api_v3.php
@@ -230,7 +230,7 @@ class Ecwid_Api_V3
 			array(
 				'timeout' => 20,
 				'headers' => array(
-					'Content-Type' => 'application/json;application/json;charest="utf-8"')
+					'Content-Type' => 'application/json;charset="utf-8"')
 			)
 		);
 
