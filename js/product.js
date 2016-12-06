23659281d6c7f76d985f1768f342122aa05abc12
Vladimir Tarutko <marvin@ecwid.com>
2016-12-06 15:57:30 +0400

    moved canonical changes to the spot when we actually know canonical

--- js/product.js
+++ js/product.js
@@ -1,17 +1,17 @@
 jQuery(document).ready(function() {
 	Ecwid.OnPageLoaded.add(function(page) {
 		if (page.type == 'PRODUCT') {
-			$canonical = jQuery('link[rel=canonical]');
-			if ($canonical.length == 0) {
-				$canonical = jQuery('<link rel="canonical">').appendTo('head');
-			}
 
 			jQuery.getJSON(
 					ecwidProduct.ajaxurl,
 					{'action': 'ecwid_get_post_link', 'product_id': page.productId},
 					function(result) {
-						$canonical.attr('href', result);
-					}
+                        $canonical = jQuery('link[rel=canonical]');
+                        if ($canonical.length == 0) {
+                            $canonical = jQuery('<link rel="canonical">').appendTo('head');
+                        }
+                        $canonical.attr('href', result);
+                    }
 			);
 		}
 	});
