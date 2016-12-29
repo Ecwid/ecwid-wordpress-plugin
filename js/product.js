6d49a95f878b09fc0d3d80544929b400cb731aba
Vladimir Tarutko <marvin@ecwid.com>
2016-12-29 12:44:15 +0400

    fixes js error

--- js/product.js
+++ js/product.js
@@ -1,18 +1,20 @@
 jQuery(document).ready(function() {
-	Ecwid.OnPageLoaded.add(function(page) {
-		if (page.type == 'PRODUCT') {
+	if (typeof Ecwid != 'undefined') {
+        Ecwid.OnPageLoaded.add(function (page) {
+            if (page.type == 'PRODUCT') {
 
-			jQuery.getJSON(
-					ecwidProduct.ajaxurl,
-					{'action': 'ecwid_get_post_link', 'product_id': page.productId},
-					function(result) {
+                jQuery.getJSON(
+                    ecwidProduct.ajaxurl,
+                    {'action': 'ecwid_get_post_link', 'product_id': page.productId},
+                    function (result) {
                         $canonical = jQuery('link[rel=canonical]');
                         if ($canonical.length == 0) {
                             $canonical = jQuery('<link rel="canonical">').appendTo('head');
                         }
                         $canonical.attr('href', result);
                     }
-			);
-		}
-	});
+                );
+            }
+        });
+    }
 });
