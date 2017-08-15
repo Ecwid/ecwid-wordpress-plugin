e4e5a296fc19d4aba376ef70853a0739396eb0ab
Vladimir Tarutko <marvin@ecwid.com>
2017-08-15 12:37:31 +0400

    Renamed ecwid_api method

--- includes/class-ecwid-api.php
+++ includes/class-ecwid-api.php
@@ -2,7 +2,7 @@
 
 
 class Ecwid_API {
-	public static function is_store_page( $page_id = 0 ) {
+	public static function page_contains_product_browser( $page_id = 0 ) {
 		require_once ECWID_PLUGIN_DIR . 'includes/class-ecwid-store-page.php';
 		
 		return Ecwid_Store_Page::is_store_page( $page_id );
