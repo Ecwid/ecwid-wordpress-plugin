505212349b5eb1c1f890d4a1652a70c949fb55c3
Vladimir Tarutko <marvin@ecwid.com>
2018-02-14 14:04:18 +0400

    added products link

--- templates/importer/complete.tpl.php
+++ templates/importer/complete.tpl.php
@@ -1,3 +1,7 @@
 <div>complete</div>
 
 <pre id="report"></pre>
+
+<p>
+    <a class="button button-primary" href="admin.php?page=<?php echo Ecwid_Admin::ADMIN_SLUG; ?>-admin-products">Go to your Ecwid Products</a>
+</p>
