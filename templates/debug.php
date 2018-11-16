6480f3acd7d5f7d280e4cd65d010227209575a79
Vladimir Tarutko <marvin@ecwid.com>
2018-11-16 13:56:35 +0400

    fixed some warnings

--- templates/debug.php
+++ templates/debug.php
@@ -51,7 +51,7 @@
 </div>
 
 <h2>Remote get test</h2>
-<div><?php var_export($remote_get_results); ?></div>
+<div><?php var_export(@$remote_get_results); ?></div>
 
 <h2>Api V3 profile test</h2>
 <div><?php var_export($api_v3_profile_results); ?></div>
@@ -94,7 +94,7 @@
 <h2>Store pages</h2>
     <?php foreach (Ecwid_Store_Page::get_store_pages_array() as $page_id): ?>
     <div>
-        <a target="_blank" href="post.php?post=<?php echo $page_id; ?>&action=edit"><?php echo get_post($page_id)->post_name; ?></a>
+        <a target="_blank" href="post.php?post=<?php echo $page_id; ?>&action=edit"><?php echo @get_post($page_id)->post_name; ?></a>
         <?php if ( $page_id == get_option( Ecwid_Store_Page::OPTION_MAIN_STORE_PAGE_ID ) ): ?>
         <b> - main</b>
         <?php endif ;?>
