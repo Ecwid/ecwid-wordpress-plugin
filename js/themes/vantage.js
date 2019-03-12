a7bf08dfaf233bc51deccfa6b92779a14d0e801f
meteor <meteor@ecwid.com>
2019-03-12 11:47:44 +0400

    PLUGINS-5644

--- js/themes/vantage.js
+++ js/themes/vantage.js
@@ -16,26 +16,31 @@
 })();
 
 Ecwid.OnAPILoaded.add( function() {
-	var s = jQuery('#scroll-to-top'),
-		sb = parseInt(s.css('bottom')),
-		sr = parseInt(s.css('right')),
-		sh = s.outerHeight(true),
-		sw = s.outerWidth(true);
+	if( jQuery('#scroll-to-top:visible').length > 0 ) {
+		var s = jQuery('#scroll-to-top'),
+			sb = parseInt(s.css('bottom')),
+			sr = parseInt(s.css('right')),
+			sh = s.outerHeight(true),
+			sw = s.outerWidth(true);
 
-	var c = jQuery('.ec-minicart'),
-		cb = c.parent().data('verticalIndent'),
-		cr = c.parent().data('horizontalIndent'),
-		cw = c.outerWidth(true);
+		var c = jQuery('.ec-minicart'),
+			cb = c.parent().data('verticalIndent'),
+			cr = c.parent().data('horizontalIndent'),
+			cw = c.outerWidth(true);
 
-	if( cr <= (sr + sw) && cb <= (sb + sh) ){
-		s.on('change',function(){
-			if( jQuery(this).hasClass('displayed') ){
-				c.css('bottom', sb + sh + 10 );
-			} else {
-				c.css('bottom', cb );
-			}
-		});
+		if( cr <= (sr + sw) && cb <= (sb + sh) ){
 
-		s.trigger('change');
+			c.css('right', sr);
+
+			s.on('change',function(){
+				if( jQuery(this).hasClass('displayed') ){
+					c.css('bottom', sb + sh + 10 );
+				} else {
+					c.css('bottom', cb );
+				}
+			});
+
+			s.trigger('change');
+		}
 	}
 });
