7620749007c787822afc9714e720de105beeda65
meteor <meteor@ecwid.com>
2019-03-12 12:18:11 +0400

    PLUGINS-5644

--- js/themes/vantage.js
+++ js/themes/vantage.js
@@ -17,30 +17,34 @@
 
 Ecwid.OnAPILoaded.add( function() {
 	if( jQuery('#scroll-to-top:visible').length > 0 ) {
-		var s = jQuery('#scroll-to-top'),
-			sb = parseInt(s.css('bottom')),
-			sr = parseInt(s.css('right')),
-			sh = s.outerHeight(true),
-			sw = s.outerWidth(true);
 
-		var c = jQuery('.ec-minicart'),
-			cb = c.parent().data('verticalIndent'),
-			cr = c.parent().data('horizontalIndent'),
-			cw = c.outerWidth(true);
+		var $scroll = jQuery('#scroll-to-top'),
+			scroll_bottom = parseInt($scroll.css('bottom')),
+			scroll_right = parseInt($scroll.css('right'));
+		
+		var $cart = jQuery('.ec-minicart'),
+			cart_bottom = $cart.parent().data('verticalIndent');
 
-		if( cr <= (sr + sw) && cb <= (sb + sh) ){
+		var rect1 = $scroll.get(0).getBoundingClientRect(),
+			rect2 = $cart.get(0).getBoundingClientRect();
 
-			c.css('right', sr);
+		var overlap = !(rect1.right < rect2.left || 
+                rect1.left > rect2.right || 
+                rect1.bottom < rect2.top || 
+                rect1.top > rect2.bottom);
 
-			s.on('change',function(){
+		if( overlap ){
+			$cart.css('right', scroll_right);
+
+			$scroll.on('change',function(){
 				if( jQuery(this).hasClass('displayed') ){
-					c.css('bottom', sb + sh + 10 );
+					$cart.css('bottom', rect1.height + scroll_bottom + 10 );
 				} else {
-					c.css('bottom', cb );
+					$cart.css('bottom', cart_bottom );
 				}
 			});
 
-			s.trigger('change');
+			$scroll.trigger('change');
 		}
 	}
 });
