c8ff4c8fceef32c4858253ae56b1fe736c94c05f
meteor <meteor@ecwid.com>
2019-03-14 09:39:21 +0400

    PLUGINS-5644

--- js/themes/vantage.js
+++ js/themes/vantage.js
@@ -15,39 +15,43 @@
 
 })();
 
-Ecwid.OnAPILoaded.add( function() {
-	if( jQuery('#scroll-to-top:visible').length > 0 ) {
+jQuery(document).ready(function() {
+    if (typeof Ecwid == 'undefined') return;
 
-		var $scroll = jQuery('#scroll-to-top'),
-			scroll_bottom = parseInt($scroll.css('bottom'));
-		
-		var $cart = jQuery('.ec-minicart'),
-			cart_bottom = $cart.parent().data('verticalIndent');
+	Ecwid.OnAPILoaded.add( function() {
+		if( jQuery('#scroll-to-top:visible').length > 0 ) {
 
-		var overlap = false,
-			overlap_init = false;
-
-		$scroll.on('change',function(){
-			var rect1 = $scroll.get(0).getBoundingClientRect(),
-				rect2 = $cart.get(0).getBoundingClientRect();
+			var $scroll = jQuery('#scroll-to-top'),
+				scroll_bottom = parseInt($scroll.css('bottom'));
 			
-			if( !overlap_init ) {
-				overlap = !(rect1.right < rect2.left || 
-	                rect1.left > rect2.right || 
-	                rect1.bottom < rect2.top || 
-	                rect1.top > rect2.bottom);
-			}
+			var $cart = jQuery('.ec-minicart'),
+				cart_bottom = $cart.parent().data('verticalIndent');
 
-			if( overlap ){
-				if( jQuery(this).hasClass('displayed') ){
-					$cart.css('bottom', rect1.height + scroll_bottom + 10 );
-					overlap_init = true;
-				} else {
-					$cart.css('bottom', cart_bottom );
+			var overlap = false,
+				overlap_init = false;
+
+			$scroll.on('change',function(){
+				var rect1 = $scroll.get(0).getBoundingClientRect(),
+					rect2 = $cart.get(0).getBoundingClientRect();
+				
+				if( !overlap_init ) {
+					overlap = !(rect1.right < rect2.left || 
+		                rect1.left > rect2.right || 
+		                rect1.bottom < rect2.top || 
+		                rect1.top > rect2.bottom);
 				}
-			}
-		});
 
-		$scroll.trigger('change');
-	}
+				if( overlap ){
+					if( jQuery(this).hasClass('displayed') ){
+						$cart.css('bottom', rect1.height + scroll_bottom + 10 );
+						overlap_init = true;
+					} else {
+						$cart.css('bottom', cart_bottom );
+					}
+				}
+			});
+
+			$scroll.trigger('change');
+		}
+	});
 });
