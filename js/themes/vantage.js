0357edf3d48a31745f379abf2265fd381b23253d
meteor <meteor@ecwid.com>
2019-03-15 16:57:00 +0400

    PLUGINS-5644

--- js/themes/vantage.js
+++ js/themes/vantage.js
@@ -21,45 +21,49 @@ jQuery(document).ready(function() {
     var scroll = '#scroll-to-top',
     	minicart = '.ec-minicart';
 
-    if( jQuery( scroll + ':visible' ).length > 0 ) {
-		Ecwid.OnCartChanged.add( function( cart ) {
+    var overlap = false,
+		overlap_init = false;
 
-			var overlap = false,
-				overlap_init = false;
+	Ecwid.OnCartChanged.add( function( cart ) {
 
-			if( !jQuery( minicart ).parent().data('showEmptyCart') && !cart.productsQuantity ) {
+		if( !jQuery( minicart ).parent().data('showEmptyCart') && !cart.productsQuantity ) {
+			jQuery( scroll ).unbind( 'change.Overlap' );
+			return;
+		}
+
+		jQuery( scroll ).on( 'change.Overlap', function() {
+
+			if( !jQuery( minicart ).length ) { 
 				jQuery( scroll ).unbind( 'change.Overlap' );
-				return;
+				return; 
 			}
 
-			jQuery( scroll ).on( 'change.Overlap', function() {
-
-				var rect1 = jQuery( this ).get(0).getBoundingClientRect(),
-					rect2 = jQuery( minicart ).get(0).getBoundingClientRect();
-				
-				if( !overlap_init ) {
-					overlap = !(rect1.right < rect2.left || 
-		                rect1.left > rect2.right || 
-		                rect1.bottom < rect2.top || 
-		                rect1.top > rect2.bottom);
-				}
-
-				if( overlap ){
-					if( jQuery( this ).hasClass( 'displayed' ) ){
-						window.ec.storefront.cart_widget_vertical_indent = rect1.height + parseInt( jQuery( this ).css( 'bottom' ) ) + 10;
-
-						overlap_init = true;
-					} else {
-						window.ec.storefront.cart_widget_vertical_indent = jQuery( minicart ).parent().data( 'verticalIndent' );
-					}
-					Ecwid.refreshConfig();
-				}
-			});
+			var rect1 = jQuery( this ).get(0).getBoundingClientRect(),
+				rect2 = jQuery( minicart ).get(0).getBoundingClientRect();
 			
-			setTimeout(function(){
-				if( jQuery( minicart ).length ) jQuery( document ).trigger('scroll');
-			}, 500);
+			if( !overlap_init ) {
+				overlap = !(rect1.right < rect2.left || 
+	                rect1.left > rect2.right || 
+	                rect1.bottom < rect2.top || 
+	                rect1.top > rect2.bottom);
+			}
 
+			if( overlap ){
+				if( jQuery( this ).hasClass( 'displayed' ) ){
+					window.ec.storefront.cart_widget_vertical_indent = rect1.height + parseInt( jQuery( this ).css( 'bottom' ) ) + 10;
+
+					overlap_init = true;
+				} else {
+					window.ec.storefront.cart_widget_vertical_indent = jQuery( minicart ).parent().data( 'verticalIndent' );
+				}
+				Ecwid.refreshConfig();
+			}
 		});
-	}
+		
+		setTimeout(function(){
+			if( jQuery( minicart ).length ) jQuery( document ).trigger('scroll');
+		}, 500);
+
+	});
+
 });
