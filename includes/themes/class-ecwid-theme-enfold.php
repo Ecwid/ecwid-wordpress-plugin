645020b67e4985273e668b87a0a07d0f435fa51d
meteor <meteor@ecwid.com>
2019-03-27 09:36:03 +0400

    PLUGINS-5684

--- includes/themes/class-ecwid-theme-enfold.php
+++ includes/themes/class-ecwid-theme-enfold.php
@@ -1,23 +1,11 @@
 <?php
 
-require_once ECWID_THEMES_DIR . '/class-ecwid-theme-base.php';
+require_once ECWID_PLUGIN_DIR . '/includes/class-ecwid-stub-renderer.php';
 
-class Ecwid_Theme_Enfold extends Ecwid_Theme_Base
-{
-	public function __construct()
-	{
-		parent::__construct();
-
-		add_action( 'wp_ajax_avia_ajax_text_to_preview', array( $this, 'disable_shortcode_content' ) );
-	}
-	
-	public function disable_shortcode_content()
-	{
-		if( isset($_REQUEST['avia_request']) && isset($_REQUEST['text']) && has_shortcode($_REQUEST['text'], 'ecwid')) {
-			echo __('To see the items you want to publish the page or turn on Preview.');
-			die();
-		}
+class Ecwid_Theme_Enfold_Stub_Renderer extends Ecwid_Stub_Renderer {
+	protected function _should_apply() {
+		return isset($_REQUEST['avia_request']) && isset($_REQUEST['text']) && has_shortcode($_REQUEST['text'], 'ecwid');
 	}
 }
 
-return new Ecwid_Theme_Enfold();
+return new Ecwid_Theme_Enfold_Stub_Renderer();
