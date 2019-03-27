3f8b470ddd5e0080668430be8d5a9c4d7946807c
meteor <meteor@ecwid.com>
2019-03-27 14:00:27 +0400

    PLUGINS-5684

--- includes/themes/class-ecwid-theme-enfold.php
+++ includes/themes/class-ecwid-theme-enfold.php
@@ -3,6 +3,9 @@
 require_once ECWID_PLUGIN_DIR . '/includes/class-ecwid-stub-renderer.php';
 
 class Ecwid_Theme_Enfold_Stub_Renderer extends Ecwid_Stub_Renderer {
+
+	public $historyjs_html4mode = false;
+
 	protected function _should_apply() {
 		return isset($_REQUEST['avia_request']);
 	}
