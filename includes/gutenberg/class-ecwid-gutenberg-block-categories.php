51db60076ea393b7298f586db1597cd6dcc06406
Vladimir Tarutko <marvin@ecwid.com>
2019-03-27 18:39:22 +0400

    categories message

--- includes/gutenberg/class-ecwid-gutenberg-block-categories.php
+++ includes/gutenberg/class-ecwid-gutenberg-block-categories.php
@@ -19,4 +19,22 @@ class Ecwid_Gutenberg_Block_Categories extends Ecwid_Gutenberg_Block_Base {
 
 		return $shortcode->render();
 	}
+	
+	public function get_params() {
+		$params = parent::get_params();
+		
+		$params['has_categories'] = true;
+		if ( ecwid_is_demo_store() ) {
+			$params['has_categories'] = false;
+		} else {
+			$api = new Ecwid_Api_V3();
+			$cats = $api->get_categories( array( 'hidden_categories' => true ) );
+
+			if ( $cats->count == 0 ) {
+				$params['has_categories'] = false;
+			}
+		}
+
+		return $params;
+	}
 }
