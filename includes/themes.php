f3bfc0ba33280ce372f1906c3e45e191aeeba050
meteor <meteor@ecwid.com>
2019-03-28 12:05:24 +0400

    PLUGINS-5644

--- includes/themes.php
+++ includes/themes.php
@@ -68,8 +68,7 @@ function ecwid_apply_theme($theme_name = null)
 		'Divi'					=> array( Ecwid_Theme_Base::PROP_USE_JS_API_FOR_CATS_NAV_MENU ),
 		'themify-music'			=> array( Ecwid_Theme_Base::PROP_AJAX_DEFER_RENDERING ),
 		'bridge'	    		=> array( Ecwid_Theme_Base::PROP_AJAX_DEFER_RENDERING ),
-		'Parallax-One'			=> array( 'css' ),
-		'vantage'				=> array( 'js' )
+		'Parallax-One'			=> array( 'css' )
 	);
 	$generic_themes = apply_filters('ecwid_generic_themes', $generic_themes);
 
