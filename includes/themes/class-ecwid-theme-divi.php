38515f857eb25db7560b95ffa30efe36147deb91
meteor <meteor@ecwid.com>
2019-04-30 16:58:49 +0400

    PLUGINS-5747

--- includes/themes/class-ecwid-theme-divi.php
+++ includes/themes/class-ecwid-theme-divi.php
@@ -6,8 +6,7 @@ class Ecwid_Divi extends Ecwid_Theme_Base
 {
 	protected $name = 'Divi';
 
-	public function __construct()
-	{
+	public function __construct() {
 		parent::__construct();
 
 		add_filter( Ecwid_Nav_Menus::FILTER_USE_JS_API_FOR_CATS_MENU, array( $this, 'filter_use_js_api_for_cats_menu' ) );
@@ -15,20 +14,26 @@ class Ecwid_Divi extends Ecwid_Theme_Base
 		if( $this->is_wireframe_view() ) {
 			add_filter( 'ecwid_scriptjs_code', '__return_false' );
 		}
+
+		if( $this->is_visual_view() ) {
+			remove_all_filters( 'ecwid_inline_js_config' );
+		}
+
+		if( isset( $_REQUEST['page_id'] ) && $this->is_visual_view() ) {
+			add_action( 'wp_footer', array( $this, 'add_scriptjs_code' ) );
+		}
 	}
 
-	public function is_wireframe_view()
-	{
-		return isset( $_REQUEST['et_fb'] ) && isset( $_REQUEST['et_bfb'] );
+	public function is_wireframe_view() {
+		return isset( $_REQUEST['et_bfb'] );
 	}
 
-	public function is_visual_views()
-	{
-		// $_POST['is_fb_preview']
-		// $_POST['shortcode']
-		// $_GET['et_pb_preview']
-		// $_GET['iframe_id']
-		return isset( $_REQUEST['et_pb_preview'] );
+	public function is_visual_view() {
+		return isset( $_REQUEST['et_fb'] ) && !isset( $_REQUEST['et_bfb'] );
+	}
+
+	public function add_scriptjs_code() {
+		echo ecwid_get_scriptjs_code();
 	}
 }
 
