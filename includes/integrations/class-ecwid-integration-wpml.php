2333f805a4f4a2309ed07e1e8d791a4b10e29c7a
meteor <meteor@ecwid.com>
2019-05-21 16:09:43 +0400

    PLUGINS-5753

--- includes/integrations/class-ecwid-integration-wpml.php
+++ includes/integrations/class-ecwid-integration-wpml.php
@@ -2,61 +2,59 @@
 
 class Ecwid_Integration_WPML
 {
-	public function __construct()
-	{
+	public function __construct() {
 		add_filter( 'ecwid_lang', array( $this, 'force_scriptjs_lang' ) );
 		add_filter( 'ecwid_relative_permalink', array( $this, 'mod_relative_permalink' ), 10, 2 );
 
-		// dynamic 
-		/*
-		add_filter( 'wpml_hreflangs', function( $wpml_hreflangs ){
-
-			if( is_array( $wpml_hreflangs ) ) {
-				$hreflang_js = 'window.ec.config.storefrontUrls.enableHreflangTags = true;';
-				$hreflang_js .= 'window.ec.config.storefrontUrls.internationalPages = {';
-				foreach( $wpml_hreflangs as $lang => $url ) {
-					$hreflang_js .= sprintf( '"%s": "%s",', $lang, $url );
-				}
-				$hreflang_js .= '};';
-			}
-
-			// add_filters( 'ecwid_inline_js_config', function( $js ){
-
-			// 	var_dump('xxx');
-			// 	var_dump($hreflang_js);
-
-			// 	return $js;
-			// });
-
-			return $wpml_hreflangs;
-		}, 10, 1 );
-		*/
-
-		// static
 		add_filter( 'wpml_hreflangs', array( $this, 'set_hreflangs' ), 10, 1 );
+		add_filter( 'wpml_hreflangs_html', array( $this, 'set_hreflangs_html'), 10, 1 );
 	}
 
-	public function set_hreflangs( $hreflang_items )
-	{
+	public function set_hreflangs( $hreflang_items ) {
 		$this->hreflang_items = $hreflang_items;
+
 		add_filter( 'ecwid_hreflangs', array( $this, 'get_hreflangs' ), 10, 1);
+		add_filter( 'ecwid_inline_js_config', array( $this, 'add_inline_js_config' ), 10, 1);
 
 		return $hreflang_items;
 	}
 
-	public function get_hreflangs()
-	{
+	public function get_hreflangs() {
 		return $this->hreflang_items;
 	}
 
-	public function force_scriptjs_lang( $lang ) 
-	{
+	public function set_hreflangs_html( $hreflang ) {
+		if( class_exists( 'Ecwid_Static_Page' ) && Ecwid_Static_Page::is_data_available() ) {
+			$ecwid_hreflang = Ecwid_Static_Page::get_href_lang_html();
+			if( $ecwid_hreflang ) {
+				return $ecwid_hreflang;
+			}
+		}
+
+		return $hreflang;
+	}
+
+	public function add_inline_js_config( $js ) {
+		if( is_array( $this->hreflang_items ) ) {
+			$js .= 'window.ec.config.storefrontUrls.enableHreflangTags = true;';
+			$js .= 'window.ec.config.storefrontUrls.internationalPages = {';
+			
+			foreach( $this->hreflang_items as $lang => $url ) {
+				$js .= sprintf( '"%s": "%s",', $lang, $url );
+			}
+
+			$js .= '};';
+		}
+
+		return $js;
+	}
+
+	public function force_scriptjs_lang( $lang ) {
 		$current_language_code = apply_filters( 'wpml_current_language', null );
 		return $current_language_code;
 	}
 
-	public function mod_relative_permalink( $default_link, $item_id )
-	{
+	public function mod_relative_permalink( $default_link, $item_id ) {
 		global $sitepress;
 
 		if ( $sitepress->get_setting( 'language_negotiation_type' ) == WPML_LANGUAGE_NEGOTIATION_TYPE_DIRECTORY ) {
