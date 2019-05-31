985fdf0aa08e9fa485cec76758704cd3b24700eb
meteor <meteor@ecwid.com>
2019-05-31 14:52:36 +0400

    PLUGINS-5736

--- includes/integrations/class-ecwid-integration-pwa.php
+++ includes/integrations/class-ecwid-integration-pwa.php
@@ -5,11 +5,32 @@ class Ecwid_Integration_PWA
 	public $cache_prefix = 'ec-store';
 
 	public function __construct() {
-		$this->register_caching_route();
+		$this->_register_routes();
 	}
 
-	public function register_caching_route() {
+	protected function _register_routes() {
+		
+		$routes = $this->_get_routes();
 
+		if( !is_array( $routes ) && empty( $routes ) ) {
+			return;
+		}
+
+		foreach ($routes as $route) {
+			wp_register_service_worker_caching_route(
+				$route['pattern'],
+				array(
+					'strategy'  => $route['strategy'],
+					'cacheName' => sprintf( '%s: %s', $this->cache_prefix, $route['cache_name'] ),
+					'plugins'   => array(
+						'expiration' => $route['expiration']
+					)
+				)
+			);
+		}
+	}
+
+	protected function _get_routes() {
 		$store_id = get_ecwid_store_id();
 
 		$stale_while_revalidate = array(
@@ -22,94 +43,61 @@ class Ecwid_Integration_PWA
 			'data.js\?ownerid=' . $store_id
 		);
 
-		wp_register_service_worker_caching_route(
-			'.*(?:' . implode( '|', $stale_while_revalidate ) . ').*$',
-			array(
-				'strategy'  => WP_Service_Worker_Caching_Routes::STRATEGY_STALE_WHILE_REVALIDATE,
-				'cacheName' => $this->cache_prefix . ': stale-while-revalidate',
-				'plugins'   => array(
-					'expiration'        => array(
-						'maxEntries'    => 100,
-						'maxAgeSeconds' => 60 * 60 * 24 * 30,
-					)
-				)
-			)
-		);
-
-
-
-		wp_register_service_worker_caching_route(
-			'.*(?:png|gif|jpg|svg)$',
-			array(
-				'strategy'  => WP_Service_Worker_Caching_Routes::STRATEGY_STALE_WHILE_REVALIDATE,
-				'cacheName' => $this->cache_prefix . ': images-cache',
-				'plugins'   => array(
-					'expiration'        => array(
-						'maxEntries'    => 100,
-						'maxAgeSeconds' => 60 * 60 * 24 * 30,
-						'purgeOnQuotaError' => true
-					)
-				)
-			)
-		);
-
-
-		wp_register_service_worker_caching_route(
-			'.*(?:ttf|woff|woff2)$',
-			array(
-				'strategy'  => WP_Service_Worker_Caching_Routes::STRATEGY_STALE_WHILE_REVALIDATE,
-				'cacheName' => $this->cache_prefix . ': fonts-cache',
-				'plugins'   => array(
-					'expiration'        => array(
-						'maxAgeSeconds' => 60 * 60 * 24 * 365,
-					)
-				)
-			)
-		);
-
-
 		$plugin_dir_name = dirname( ECWID_PLUGIN_BASENAME );
 
-		wp_register_service_worker_caching_route(
-			'.*\/' . $plugin_dir_name . '\/.*(?:css|js).*$',
+		$routes = array(
+			
 			array(
-				'strategy'  => WP_Service_Worker_Caching_Routes::STRATEGY_STALE_WHILE_REVALIDATE,
-				'cacheName' => $this->cache_prefix . ': plugin-cache',
-				'plugins'   => array(
-					'expiration'        => array(
-						'maxAgeSeconds' => 60 * 60 * 24 * 30,
-					)
+				'pattern' => '.*(?:' . implode( '|', $stale_while_revalidate ) . ').*$',
+				'cache_name' => 'stale-while-revalidate',
+				'strategy' => WP_Service_Worker_Caching_Routes::STRATEGY_STALE_WHILE_REVALIDATE,
+				'expiration' => array(
+					'maxEntries'    => 100,
+					'maxAgeSeconds' => 60 * 60 * 24 * 30
 				)
-			)
+			),
+			
+			array(
+				'pattern' => '.*(?:png|gif|jpg|svg)$',
+				'cache_name' => 'images-cache',
+				'strategy' => WP_Service_Worker_Caching_Routes::STRATEGY_STALE_WHILE_REVALIDATE,
+				'expiration' => array(
+					'maxEntries'    => 100,
+					'maxAgeSeconds' => 60 * 60 * 24 * 30,
+					'purgeOnQuotaError' => true
+				)
+			),
+			
+			array(
+				'pattern' => '.*(?:ttf|woff|woff2)$',
+				'cache_name' => 'fonts-cache',
+				'strategy' => WP_Service_Worker_Caching_Routes::STRATEGY_STALE_WHILE_REVALIDATE,
+				'expiration' => array(
+					'maxAgeSeconds' => 60 * 60 * 24 * 365
+				)
+			),
+			
+			array(
+				'pattern' => '.*\/' . $plugin_dir_name . '\/.*(?:css|js).*$',
+				'cache_name' => 'plugin-cache',
+				'strategy' => WP_Service_Worker_Caching_Routes::STRATEGY_STALE_WHILE_REVALIDATE,
+				'expiration' => array(
+					'maxAgeSeconds' => 60 * 60 * 24 * 30
+				)
+			),
+			
+			array(
+				'pattern' => '.*script.js\?' . $store_id . '.*$',
+				'cache_name' => 'network-first',
+				'strategy' => WP_Service_Worker_Caching_Routes::STRATEGY_NETWORK_FIRST,
+				'expiration' => array(
+					'maxAgeSeconds' => 60 * 60 * 24 * 30
+				)
+			),
+
 		);
 
-
-		wp_register_service_worker_caching_route(
-			'\/(\?lang=.*\&from_admin)?$',
-			array(
-				'strategy'  => WP_Service_Worker_Caching_Routes::STRATEGY_NETWORK_FIRST,
-				'cacheName' => $this->cache_prefix . ': home-page',
-				'plugins'   => array(
-					'expiration'        => array(
-						'maxAgeSeconds' => 60 * 60 * 24 * 30,
-					)
-				)
-			)
-		);
-
-		$store_id = get_ecwid_store_id();
-		wp_register_service_worker_caching_route(
-			'.*script.js\?' . $store_id . '.*$',
-			array(
-				'strategy'  => WP_Service_Worker_Caching_Routes::STRATEGY_NETWORK_FIRST,
-				'cacheName' => $this->cache_prefix . ': network-first',
-				'plugins'   => array(
-					'expiration'        => array(
-						'maxAgeSeconds' => 60 * 60 * 24 * 30,
-					)
-				)
-			)
-		);
+		return $routes;
 	}
 
 }
