b7662c08af67c5912f318d097a6ff6d081bcae60
Ilnur Basyrov <meteor@ecwid.com>
2019-07-22 18:13:09 +0400

    PLUGINS-39

--- includes/class-ecwid-static-page.php
+++ includes/class-ecwid-static-page.php
@@ -266,8 +266,8 @@ class Ecwid_Static_Page {
 	
 	public static function is_enabled_static_home_page() {
 		$html_catalog_params = Ecwid_Seo_Links::maybe_extract_html_catalog_params();
-		$is_valid_page = empty( $html_catalog_params );
-		if( !$is_valid_page ) {
+		$is_home_page = empty( $html_catalog_params );
+		if( !$is_home_page ) {
 			return false;
 		}
 
