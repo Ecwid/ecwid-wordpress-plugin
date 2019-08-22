dba57b514779a7492fcbd73bae55266b13dd8c9d
Ilnur Basyrov <meteor@ecwid.com>
2019-08-22 15:52:30 +0400

    PLUGINS-5916

---
+++ lib/ecwid_product_api.php
@@ -0,0 +1,6 @@
+<?
+/*
+* This file is deprecated. It cannot be directly connected in your plugins. To work with the Ecwid store, use the official API: https://developers.ecwid.com/api-documentation
+* With the help of Ecwid API, you can create an application that will extend Ecwid functionality in various ways.
+*/
+?>
