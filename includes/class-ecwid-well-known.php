89a6a829a36c4498e0e81bdd3a9384bc840712c5
Ilnur Basyrov <meteor@ecwid.com>
2019-11-11 23:05:18 +0400

    PLUGINS-6001

--- includes/class-ecwid-well-known.php
+++ includes/class-ecwid-well-known.php
@@ -10,7 +10,7 @@ class Ecwid_Well_Known {
 		add_action( 'generate_rewrite_rules', array($this, 'rewrite_rules'), 1000 );
 
 		// if permalinks are disabled
-		// add_action( 'permalink_structure_changed', array($this, 'hook_permalink_structure_changed'), 10, 2 );
+		add_action( 'permalink_structure_changed', array($this, 'hook_permalink_structure_changed'), 10, 2 );
 		// add_action( 'activated_plugin', array($this, 'hook_activated_plugin'), 10, 2 );
 
 		// Well-Known URIs
@@ -24,7 +24,7 @@ class Ecwid_Well_Known {
 
 	public function rewrite_rules($wp_rewrite) {
 		$well_known_rules = array(
-			'.well-known/(.+)' => 'index.php?ec-well-known='.$wp_rewrite->preg_index(1),
+			'.well-known/(.+)' => 'index.php?ec-well-known='.$wp_rewrite->preg_index(1)
 		);
 		$wp_rewrite->rules = $well_known_rules + $wp_rewrite->rules;
 	}
@@ -38,12 +38,10 @@ class Ecwid_Well_Known {
 		}
 	}
 
-	/*
+
 	public function save_mod_rewrite_rules() {
 		if( !Ecwid_Seo_Links::is_feature_available() ) {
-			error_log( 'save_mod_rewrite_rules' );
-
-			$home_path     = get_home_path();
+			$home_path = get_home_path();
 			$htaccess_file = $home_path . '.htaccess';
 
 			$rules = array();
@@ -52,9 +50,8 @@ class Ecwid_Well_Known {
 			$rules[] = 'RewriteRule ^\.well-known/(.+)$ index.php?ec-well-known=$1 [L]';
 			$rules[] = '</IfModule>';
 
-			// $brand = Ecwid_Config::get_brand();
-			// insert_with_markers( $htaccess_file, $brand, $rules );
-			insert_with_markers( $htaccess_file, 'WordPress', $rules );
+			$brand = Ecwid_Config::get_brand();
+			insert_with_markers( $htaccess_file, $brand, $rules );
 		}
 	}
 
@@ -62,16 +59,9 @@ class Ecwid_Well_Known {
 		$this->save_mod_rewrite_rules();
 	}
 
-	public function hook_activated_plugin( $plugin, $network_wide ) {
-
-		error_log( $plugin );
-		error_log( class_exists('Ecwid_Well_Know') );
-
-		if( class_exists('Ecwid_Well_Know') ) {
-			$this->save_mod_rewrite_rules();
-		}
-	}
-	*/
+	// public function hook_activated_plugin( $plugin, $network_wide ) {
+	// 	$this->save_mod_rewrite_rules();
+	// }
 
 	public function apple_pay_verification( $query_vars ) {
 		$api = new Ecwid_Api_V3();
