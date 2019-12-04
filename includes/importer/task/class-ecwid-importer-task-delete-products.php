bc8cef40936f8e8e6cac36eafea04f39752ca708
Ilnur Basyrov <meteor@ecwid.com>
2019-12-04 12:55:04 +0400

    PLUGINS-5975

--- includes/importer/task/class-ecwid-importer-task-delete-products.php
+++ includes/importer/task/class-ecwid-importer-task-delete-products.php
@@ -5,13 +5,42 @@ class Ecwid_Importer_Task_Delete_Products extends Ecwid_Importer_Task
 	public static $type = 'delete_products';
 
 	public function execute( Ecwid_Importer $exporter, array $data ) {
-		$api = new Ecwid_Api_V3();
-
+		
 		$ids = $data['ids'];
+		$exporter->clear_batch();
 
-		$result = $api->delete_products( $ids );
+		foreach ( $ids as $id ) {
+			$api = new Ecwid_Api_V3();
+			$batch_item = null;
 
-		return self::_process_api_result( $result, $data );
+			$batch_item_id = self::$type . '|' . $id;
+			$batch_item = $api->batch_delete_product( $id, $batch_item_id );
+
+			$exporter->append_batch( $batch_item );
+		}
+
+		$batch = $exporter->get_batch();
+		if( count( $batch ) ) {
+
+			$api = new Ecwid_Api_V3();
+			$result = $api->create_batch( $batch );
+
+			if( $result['response']['code'] == '200' ) {
+
+				$data = json_decode( $result['body'] );
+				$ticket = $data->ticket;
+
+				$exporter->append_child(
+					Ecwid_Importer_Task_Batch_Status::build(
+						array( 'ticket' => $ticket )
+					)
+				);
+
+				$exporter->clear_batch();
+			}
+		}
+
+		return $this->_result_success();
 	}
 
 	public static function build( array $ids ) {
