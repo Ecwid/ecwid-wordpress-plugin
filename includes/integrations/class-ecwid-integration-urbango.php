4315ac72f3e78d685a77986d44b88239d69d029b
Ilnur Basyrov <meteor@ecwid.com>
2020-03-05 12:27:51 +0400

    PLUGINS-6123

--- includes/integrations/class-ecwid-integration-urbango.php
+++ includes/integrations/class-ecwid-integration-urbango.php
@@ -8,10 +8,19 @@ class Ecwid_Integration_UrbanGo
 	}
 
 	public function rewrite_rules($wp_rewrite) {
-		
+
 		$patterns = Ecwid_Seo_Links::get_seo_links_patterns();
 		foreach ( $patterns as $pattern ) {
+
 			$theme_rules['listing-item/([^/]+)/' . $pattern] = 'index.php?listing-item=$matches[1]';
+
+			if( function_exists('urbango_edge_options') ) {
+				$custom_slug = urbango_edge_options()->getOptionValue( 'listing_single_slug' );
+
+				if( !empty($custom_slug) ) {
+					$theme_rules[ $custom_slug . '/([^/]+)/' . $pattern] = 'index.php?listing-item=$matches[1]';
+				}
+			}
 		}
 
 		$wp_rewrite->rules = $theme_rules + $wp_rewrite->rules;
