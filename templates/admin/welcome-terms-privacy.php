18d0b066c1079fe94de0e6adebf0be7f995ac18a
Ilnur Basyrov <meteor@ecwid.com>
2020-04-21 15:52:03 +0400

    fixed comma in template

--- templates/admin/welcome-terms-privacy.php
+++ templates/admin/welcome-terms-privacy.php
@@ -4,7 +4,7 @@
 	echo sprintf( 
 		__( 'By continuing, you agree to the <a %s>Terms of Service</a> and <a %s>Privacy Policy</a>.', 'ecwid-shopping-cart' ),
 		'href="http://www.ecwid.com/terms-of-service" target="_blank"',
-		'href="https://www.ecwid.com/privacy-policy" target="_blank"',
+		'href="https://www.ecwid.com/privacy-policy" target="_blank"'
 	);
 	?>
 </div>
