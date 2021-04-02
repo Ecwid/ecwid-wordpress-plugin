1b73f4a6b322d63df1a98fa5342079f74dec8645
meteor <meteor@ecwid.com>
2021-04-02 22:56:44 +0400

    PLUGINS-6363

--- includes/integrations/class-ecwid-integration-wpml.php
+++ includes/integrations/class-ecwid-integration-wpml.php
@@ -15,6 +15,22 @@ class Ecwid_Integration_WPML
 		add_filter( 'ecwid_lang', array( $this, 'force_scriptjs_lang' ) );
 		add_filter( 'wpml_hreflangs', array( $this, 'filter_hreflangs' ), 10, 1 );
 		add_filter( 'wpml_hreflangs_html', array( $this, 'filter_hreflangs_html'), 10, 1 );
+
+		add_filter( 'ecwid_nav_categories_posts', array( $this, 'filter_ecwid_menu_items'), 10, 1 );
+	}
+
+	public function filter_ecwid_menu_items($posts) {
+
+		$lang = apply_filters( 'wpml_current_language', null );
+
+		foreach ($posts as $key => $post) {
+
+			if( !empty($post->ecwid_name_translated->{$lang}) ) {
+				$posts[$key]->title = $post->ecwid_name_translated->{$lang};
+			}
+		}
+
+		return $posts;
 	}
 	
 	public function mod_rewrite_rules( $link, $page_id ) {
