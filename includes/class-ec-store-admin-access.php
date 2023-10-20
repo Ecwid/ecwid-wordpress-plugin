5e52b873772e7c93934c42b9cbf361ca2b1adbe9
meteor <meteor@ecwid.com>
2023-10-20 15:36:30 +0400

    PLUGINS-6815

--- includes/class-ec-store-admin-access.php
+++ includes/class-ec-store-admin-access.php
@@ -17,46 +17,57 @@ class Ec_Store_Admin_Access {
 		// if ( current_user_can( 'manage_options', $user_id ) && wp_verify_nonce( $_POST['_wp_nonce'] )
 		if ( ! empty( $_POST['ec_store_admin_access'] ) ) { // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.NonceVerification.Missing
 			$user = new WP_User( $user_id );
-			$user->add_role( self::CAP_MANAGE_CONTROL_PANEL );
+			$user->add_cap( self::CAP_MANAGE_CONTROL_PANEL, true );
 		} else {
 			$user = new WP_User( $user_id );
-			$user->remove_role( self::CAP_MANAGE_CONTROL_PANEL );
+			$user->remove_cap( self::CAP_MANAGE_CONTROL_PANEL );
 		}
+	}
+
+	public static function has_scope( $user_id = null ) {
+		if ( empty( $user_id ) ) {
+			return current_user_can( self::CAP_MANAGE_CONTROL_PANEL );
+		} else {
+			return user_can( $user_id, self::CAP_MANAGE_CONTROL_PANEL );
+		}
+
+		return true;
 	}
 
 	public function print_custom_user_profile_fields( $user ) { //phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
 
-		$checked = current_user_can( self::CAP_MANAGE_CONTROL_PANEL );
+		$checked = self::has_scope( $user->ID );
 
 		?>
-		<div>&nbsp;</div>
-		<h2 class="heading">
-			<?php
-			/* translators: %s: plugin brand */
-			echo esc_html( sprintf( __( '%s Control Panel', 'ecwid-shopping-cart' ), Ecwid_Config::get_brand() ) );
-			?>
-		</h2>
-		<table class="form-table">
-		<tr>
-			<th><?php echo esc_html( __( 'Access', 'ecwid-shopping-cart' ) ); ?></th>
-			<td>
-				<label for="ec_store_admin_access">
-					<input 
-						type="checkbox"
-						name="ec_store_admin_access"
-						id="ec_store_admin_access"
-						value="1"
-						<?php echo ( $checked ) ? 'checked' : ''; ?>
-					/>
-					<?php
-					/* translators: %s: plugin brand */
-					echo esc_html( sprintf( __( 'Allow the current user to access the %s Control Panel.', 'ecwid-shopping-cart' ), Ecwid_Config::get_brand() ) );
-					?>
-				</label>
-				<a href="https://support.ecwid.com/hc/en-us/articles/207101259?utm_source=wp-plugin#can-i-restrict-access-to-the-ecwid-admin-panel-to-certain-users-" target="_blank"><?php echo esc_html( __( 'More information', 'ecwid-shopping-cart' ) ); ?></a>
-			</td>
-		</tr>
-		</table>
+		<div id="ec-store-control-panel">&nbsp;
+			<h2 class="heading">
+				<?php
+				/* translators: %s: plugin brand */
+				echo esc_html( sprintf( __( '%s Control Panel', 'ecwid-shopping-cart' ), Ecwid_Config::get_brand() ) );
+				?>
+			</h2>
+			<table class="form-table">
+			<tr>
+				<th><?php echo esc_html( __( 'Access', 'ecwid-shopping-cart' ) ); ?></th>
+				<td>
+					<label for="ec_store_admin_access">
+						<input 
+							type="checkbox"
+							name="ec_store_admin_access"
+							id="ec_store_admin_access"
+							value="1"
+							<?php echo ( $checked ) ? 'checked' : ''; ?>
+						/>
+						<?php
+						/* translators: %s: plugin brand */
+						echo esc_html( sprintf( __( 'Allow the current user to access the %s Control Panel.', 'ecwid-shopping-cart' ), Ecwid_Config::get_brand() ) );
+						?>
+					</label>
+					<a href="https://support.ecwid.com/hc/en-us/articles/207101259?utm_source=wp-plugin#can-i-restrict-access-to-the-ecwid-admin-panel-to-certain-users-" target="_blank"><?php echo esc_html( __( 'More information', 'ecwid-shopping-cart' ) ); ?></a>
+				</td>
+			</tr>
+			</table>
+		</div>
 		<?php
 	}
 }
