10f8d4282adea2df21a1a35cf3bc008ecdf54b5e
meteor <meteor@ecwid.com>
2023-10-21 18:34:04 +0400

    PLUGINS-6815

--- includes/class-ec-store-admin-access.php
+++ includes/class-ec-store-admin-access.php
@@ -3,6 +3,7 @@
 class Ec_Store_Admin_Access {
 
 	const CAP_MANAGE_CONTROL_PANEL = 'ec_store_manage_control_panel';
+	// const CAP_GRANT_ACCESS         = 'ec_store_can_grant_access';
 
 	public function __construct() {
 		add_action( 'edit_user_profile', array( $this, 'print_custom_user_profile_fields' ) );
@@ -15,26 +16,65 @@ class Ec_Store_Admin_Access {
 	public function save_custom_user_profile_fields( $user_id ) {
 		// TODO: check if user has access to edit this role
 		// if ( current_user_can( 'manage_options', $user_id ) && wp_verify_nonce( $_POST['_wp_nonce'] )
+
+		$user = new WP_User( $user_id );
+
 		if ( ! empty( $_POST['ec_store_admin_access'] ) ) { // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.NonceVerification.Missing
-			$user = new WP_User( $user_id );
 			$user->add_cap( self::CAP_MANAGE_CONTROL_PANEL, true );
 		} else {
-			$user = new WP_User( $user_id );
-			$user->remove_cap( self::CAP_MANAGE_CONTROL_PANEL );
+			$user->add_cap( self::CAP_MANAGE_CONTROL_PANEL, false );
 		}
 	}
 
 	public static function has_scope( $user_id = null ) {
+		$has_scope = false;
+
 		if ( empty( $user_id ) ) {
-			return current_user_can( self::CAP_MANAGE_CONTROL_PANEL );
+			$has_scope = current_user_can( self::CAP_MANAGE_CONTROL_PANEL );
 		} else {
-			return user_can( $user_id, self::CAP_MANAGE_CONTROL_PANEL );
+			$has_scope = user_can( $user_id, self::CAP_MANAGE_CONTROL_PANEL );
 		}
 
-		return true;
+		if ( ! $has_scope && self::is_need_set_access_by_default( $user_id ) ) {
+			$has_scope = true;
+		}
+
+		return $has_scope;
+	}
+
+	public static function is_need_set_access_by_default( $user_id ) {
+
+		$user     = new WP_User( $user_id );
+		$all_caps = $user->get_role_caps();
+
+		$cap_not_changed_before = ! isset( $all_caps[ self::CAP_MANAGE_CONTROL_PANEL ] );
+		$is_old_installation    = ecwid_migrations_is_original_plugin_version_older_than( '6.12.4' );
+
+		if ( $cap_not_changed_before && $is_old_installation ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public function can_grant_access() {
+
+		if ( is_super_admin() ) {
+			return true;
+		}
+
+		if ( current_user_can( self::CAP_MANAGE_CONTROL_PANEL ) ) {
+			return true;
+		}
+
+		return false;
 	}
 
 	public function print_custom_user_profile_fields( $user ) { //phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
+
+		if ( ! $this->can_grant_access() ) {
+			return false;
+		}
 
 		$checked = self::has_scope( $user->ID );
 
