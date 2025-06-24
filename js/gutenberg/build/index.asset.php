1e700641c9177ab5bef7126622d7267d953709c4
Ilnur Basyrov <ilnur.basyrov@lightspeedhq.com>
2025-06-24 15:12:51 +0400

    Fixed distignore and builded gutenberg blocks

--- js/gutenberg/build/index.asset.php
+++ js/gutenberg/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '48597db797c8c57c6499');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fbfb9331d768fd33bb74');
